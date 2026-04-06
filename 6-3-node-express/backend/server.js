f56d6393bce43bb244e21086fd869859b0053c91
YAQIN SHAWKAN <s202255400@kfupm.edu.sa>
2026-04-06 13:45:14 +0300

    TODO 6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -15,8 +15,15 @@ app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
 
+app.get("/", (req, res) => {
+     res.send("plain text response from root route");
+   });
 
 // TODO 6.2: Create "/api/quote" route
 
+app.get("/api/quote", (req, res) => {
+     const quote = getRandomQuote();
+     res.json({ quote });
+   });
 
 // TODO 7: Start server using app.listen
